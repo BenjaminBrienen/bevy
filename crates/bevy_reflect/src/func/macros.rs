d7cef7b65605953447d47cf50fe7527485d7e12a
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2024-10-15 07:08:49 +0200

    fix f&r error

--- crates/bevy_reflect/src/func/macros.rs
+++ crates/bevy_reflect/src/func/macros.rs
@@ -19,7 +19,7 @@
 /// impl_function_traits!(foo::bar::Baz<T, U>; <T: Clone, U>);
 /// ```
 ///
-/// For generic const parameters, they must be given in square brackets (`[` and [`Commands`]`):
+/// For generic const parameters, they must be given in square brackets (`[` and `]`):
 ///
 /// ```ignore
 /// impl_function_traits!(foo::bar::Baz<T, N>; <T> [const N: usize]);
